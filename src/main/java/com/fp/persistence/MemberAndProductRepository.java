6a54682ed84e8290ce522fa705b4a821fe0f94d9
bbyuggyu <bbyuggyu@gmail.com>
2022-08-24 02:55:32 +0900

    docs: Add Repository Annotation
    @Repository

--- src/main/java/com/fp/persistence/MemberAndProductRepository.java
+++ src/main/java/com/fp/persistence/MemberAndProductRepository.java
@@ -2,6 +2,8 @@ package com.fp.persistence;
 
 import com.fp.domain.MemberAndProduct;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface MemberAndProductRepository extends JpaRepository<MemberAndProduct, Long> {
 }
--- src/main/java/com/fp/persistence/MemberRepository.java
+++ src/main/java/com/fp/persistence/MemberRepository.java
@@ -2,6 +2,8 @@ package com.fp.persistence;
 
 import com.fp.domain.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface MemberRepository extends JpaRepository<Member, Long> {
 }
