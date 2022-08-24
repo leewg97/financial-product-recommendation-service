6ab8327f0cc48d534af4aeb928020406a84fd627
Chang1235 <dlckdgml1235@naver.com>
2022-08-24 21:27:22 +0900

    feat: 맞춤상품 추천 서비스 구현

--- src/main/java/com/fp/persistence/ProductRepository.java
+++ src/main/java/com/fp/persistence/ProductRepository.java
@@ -2,8 +2,14 @@ package com.fp.persistence;
 
 import com.fp.domain.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface ProductRepository extends JpaRepository<Product, Long> {
+    List<Product> findBySupporterAmountGreaterThanEqualAndSupporterRegion(int supporterAmount,String region);
+
 }
