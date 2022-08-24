675912b6bd38ea3bab1991907d6e9f9fe650a262
leewg97 <leewg97@naver.com>
2022-08-25 00:18:19 +0900

    refactor : @RequiredArgsConstructor 을 이용하여 의존성 주입

--- src/main/java/com/fastcampus/service/MemberService.java
+++ src/main/java/com/fastcampus/service/MemberService.java
@@ -1,21 +1,18 @@
-package com.fp.service;
+package com.fastcampus.service;
 
-import com.fp.domain.Member;
-import com.fp.persistence.MemberRepository;
+import com.fastcampus.domain.Member;
+import com.fastcampus.persistence.MemberRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
 import java.util.Optional;
 
 @Service
+@RequiredArgsConstructor
 public class MemberService {
 
-    // 생성자 주입
     private final MemberRepository memberRepository;
-
-    public MemberService(MemberRepository memberRepository) {
-        this.memberRepository = memberRepository;
-    }
 
     // 회원 등록
     @Transactional
