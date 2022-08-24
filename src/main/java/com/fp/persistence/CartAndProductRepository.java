36ace4c6159739a6b56d4b6982068b00287c87e2
bbyuggyu <bbyuggyu@gmail.com>
2022-08-24 16:50:52 +0900

    Remove: Remove File CartAndProductRepository

--- src/main/java/com/fp/persistence/CartAndProductRepository.java
+++
@@ -1,9 +0,0 @@
-package com.fp.persistence;
-
-import com.fp.domain.CartAndProduct;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface CartAndProductRepository extends JpaRepository<CartAndProduct, Long> {
-}
