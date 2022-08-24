e3b2342d41c6bb2c838d48b0ba2b9f8972f33d62
bbyuggyu <bbyuggyu@gmail.com>
2022-08-24 21:31:34 +0900

    Refactor: Modify the return value after registering as a member
    회원가입 후 json객체로 리턴하는 코드로 수정

--- src/main/java/com/fp/web/controller/MemberController.java
+++ src/main/java/com/fp/web/controller/MemberController.java
@@ -2,6 +2,11 @@ package com.fp.web.controller;
 
 import com.fp.domain.Member;
 import com.fp.service.MemberService;
+import com.fp.web.api.DefaultRes;
+import com.fp.web.api.ResponseMessage;
+import com.fp.web.api.StatusCode;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -19,15 +24,15 @@ public class MemberController {
 
     // 회원가입 처리
     @PostMapping("/register")
-    public @ResponseBody String insertUser(@RequestBody Member member) {
+    public @ResponseBody ResponseEntity<Member> insertUser(@RequestBody Member member) {
         Member findMember = memberService.getMember(member.getEmail());
 
         if(findMember.getEmail() == null) {
             memberService.insertUser(member);
             System.out.println(member);
-            return member.getEmail() + " 회원 가입 성공";
+            return new ResponseEntity(DefaultRes.res(StatusCode.OK, ResponseMessage.CREATED_USER, member), HttpStatus.OK);
         } else {
-            return member.getEmail() + " 이미 존재하는 이메일입니다.";
+            return new ResponseEntity(DefaultRes.res(StatusCode.CONFLICT, ResponseMessage.CREATED_FAIL, member.getEmail() + "은 이미 사용중인 이메일입니다."), HttpStatus.OK);
         }
     }
 
