f309b28a1addfa17e5547f78c72693033db1bd2d
WindowH22 <75984536+WindowH22@users.noreply.github.com>
2022-08-24 22:14:15 +0900

    Update ProductRepository.java

--- src/main/java/com/fp/persistence/ProductRepository.java
+++ src/main/java/com/fp/persistence/ProductRepository.java
@@ -2,7 +2,10 @@ package com.fp.persistence;
 
 import com.fp.domain.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
+import java.util.List;
+
 
 public interface ProductRepository extends JpaRepository<Product, Long> {
+    List<Product> findBySupporterAmountGreaterThanEqualAndSupporterRegion(int supporterAmount,String region);
+
 }
