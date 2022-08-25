6e09d26927b6e5d1356cec3ae0f9f83e0d48ff45
bbyuggyu <bbyuggyu@gmail.com>
2022-08-25 14:07:24 +0900

    Refactor: Modify the return value after registering as a member
    회원가입 후 json객체로 리턴하는 코드로 수정

--- src/main/java/com/fastcampus/web/controller/MemberController.java
+++ src/main/java/com/fastcampus/web/controller/MemberController.java
@@ -2,7 +2,12 @@ package com.fastcampus.web.controller;
 
 import com.fastcampus.domain.Member;
 import com.fastcampus.service.MemberService;
+import com.fastcampus.web.api.DefaultRes;
+import com.fastcampus.web.api.ResponseMessage;
+import com.fastcampus.web.api.StatusCode;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -17,15 +22,15 @@ public class MemberController {
 
     // 회원가입 처리
     @PostMapping("/register")
-    public @ResponseBody String insertUser(@RequestBody Member member) {
+    public @ResponseBody ResponseEntity insertUser(@RequestBody Member member) {
         Member findMember = memberService.getMember(member.getEmail());
 
         if(findMember.getEmail() == null) {
             memberService.insertUser(member);
             System.out.println(member);
-            return member.getEmail() + " 회원 가입 성공";
+            return new ResponseEntity(DefaultRes.res(StatusCode.OK, ResponseMessage.CREATED_USER, member), HttpStatus.OK);
         } else {
-            return member.getEmail() + " 이미 존재하는 이메일입니다.";
+            return new ResponseEntity(DefaultRes.res(StatusCode.CONFLICT, ResponseMessage.CREATED_FAIL, member.getEmail() + "은 이미 존재하는 이메일입니다."), HttpStatus.OK);
         }
     }
 
