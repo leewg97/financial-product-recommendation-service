b848464d283bf6794def601780169cbba2e5969f
Chang1235 <dlckdgml1235@naver.com>
2022-08-26 01:29:53 +0900

    feat: innerclass로 ProductDto 재구현

--- src/main/java/com/fastcampus/web/dto/ProductDto.java
+++ src/main/java/com/fastcampus/web/dto/ProductDto.java
@@ -1,6 +1,8 @@
 package com.fastcampus.web.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import javax.persistence.EnumType;
 import javax.persistence.Enumerated;
@@ -8,10 +10,23 @@ import javax.persistence.Enumerated;
 @Data
 public class ProductDto {
 
-    // 검색 관련 변수
-    @Enumerated(value = EnumType.STRING)
-    private SearchCondition searchCondition;
-
-    private String searchKeyword;
-    
+    @Data
+    @AllArgsConstructor
+    @NoArgsConstructor
+    public static class Request {
+        // 검색 관련 변수
+        @Enumerated(value = EnumType.STRING)
+        private SearchCondition searchCondition;
+        private String searchKeyword;
+    }
+    @Data
+    @AllArgsConstructor
+    public static class Response {
+        private Long id;
+        private String productName; // 상품 이름
+        private String productContent; // 상품 내용
+        private String supporterName; // 지원 은행
+        private String supporterRegion; // 지원 지역
+        private int supporterAmount; // 지원 금액
+    }
 }
