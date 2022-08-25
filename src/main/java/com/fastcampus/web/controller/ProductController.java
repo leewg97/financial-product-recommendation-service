d41ff6f0594400dabe1539c82d74abc38b9161bb
Chang1235 <dlckdgml1235@naver.com>
2022-08-25 15:57:43 +0900

    feat: Add getProduct func

--- src/main/java/com/fastcampus/web/controller/ProductController.java
+++ src/main/java/com/fastcampus/web/controller/ProductController.java
@@ -1,12 +1,21 @@
 package com.fastcampus.web.controller;
 
 import com.fastcampus.service.ProductService;
+import com.fastcampus.web.api.DefaultRes;
+import com.fastcampus.web.api.ResponseMessage;
+import com.fastcampus.web.api.StatusCode;
+import com.fastcampus.web.dto.ProductDto;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @Controller
 @RequiredArgsConstructor
+@RequestMapping("/products")
 public class ProductController {
 
     private final ProductService productService;
@@ -15,6 +24,16 @@ public class ProductController {
     public String customProduct(Long id){
         productService.customProduct(2L);
         return "good";
+    }
+
+
+    // 상품 검색
+
+
+    // 상품 조회
+    @GetMapping("/{id}")
+    public @ResponseBody ResponseEntity getProduct(@PathVariable long id) throws Exception {
+        return new ResponseEntity(DefaultRes.res(StatusCode.OK, ResponseMessage.GET_PRODUCT,productService.getProduct(id)), HttpStatus.OK);
     }
 
 }
