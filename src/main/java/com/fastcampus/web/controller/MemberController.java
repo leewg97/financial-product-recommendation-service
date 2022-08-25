7de6f40d40797487f3c54a4beb08a419d73bed78
bbyuggyu <bbyuggyu@gmail.com>
2022-08-25 13:46:55 +0900

    refactor : @RequiredArgsConstructor 을 이용하여 의존성 주입

--- src/main/java/com/fastcampus/web/controller/MemberController.java
+++ src/main/java/com/fastcampus/web/controller/MemberController.java
@@ -2,20 +2,18 @@ package com.fastcampus.web.controller;
 
 import com.fastcampus.domain.Member;
 import com.fastcampus.service.MemberService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
+@RequiredArgsConstructor
 public class MemberController {
 
     // 생성자 주입
     private final MemberService memberService;
-
-    public MemberController(MemberService memberService) {
-        this.memberService = memberService;
-    }
 
     // 회원가입 처리
     @PostMapping("/register")
