01e84dc8677948ec0a41695b54d117a6bd9918d6
leewg97 <leewg97@naver.com>
2022-08-26 20:05:04 +0900

    feat : Add ResponseMessage functionality

--- src/main/java/com/fastcampus/web/api/ResponseMessage.java
+++ src/main/java/com/fastcampus/web/api/ResponseMessage.java
@@ -9,10 +9,24 @@ public class ResponseMessage {
     public static final String CREATED_FAIL = "회원 가입 실패";
     public static final String UPDATE_USER = "회원 정보 수정 성공";
     public static final String DELETE_USER = "회원 탈퇴 성공";
+
     public static final String INTERNAL_SERVER_ERROR = "서버 내부 에러";
     public static final String DB_ERROR = "데이터베이스 에러";
     public static final String GET_PRODUCT_LIST = "상품목록 조회 성공";
     public static final String GET_PRODUCT = "상품조회 성공";
     public static final String GET_CUSTOM_PRODUCT_LIST = "맞춤상품 조회 성공";
     public static final String GET_PRODUCT_SEARCH = "상품검색 성공";
+
+    public static final String ADD_CART = "장바구니 등록 성공";
+    public static final String READ_CART = "장바구니 목록 조회 성공";
+    public static final String EXIST_CART = "장바구니에 이미 존재";
+    public static final String NOT_FOUNT_CART = "장바구니 목록 없음";
+    public static final String DELETE_CART = "장바구니 삭제 성공";
+    public static final String ASK_CART = "장바구니 신청 성공";
+
+    public static final String ADD_BOOKMARK = "찜 등록 성공";
+    public static final String READ_BOOKMARK = "찜 목록 조회 성공";
+    public static final String NOT_FOUND_BOOKMARK = "찜 목록 없음";
+    public static final String DELETE_BOOKMARK = "찜 목록 삭제 성공";
+
 }
