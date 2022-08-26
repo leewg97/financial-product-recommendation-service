0c5e8298af34e433e0f4a195fd062f0ffb73f684
leewg97 <leewg97@naver.com>
2022-08-26 20:02:52 +0900

    chore : 주석 수정

--- src/main/java/com/fastcampus/web/controller/IndexController.java
+++ src/main/java/com/fastcampus/web/controller/IndexController.java
@@ -1,16 +1,9 @@
 package com.fastcampus.web.controller;
 
-import com.fastcampus.domain.Product;
 import com.fastcampus.service.ProductService;
-import com.fastcampus.web.dto.ProductDto;
-import com.fastcampus.web.dto.SearchCondition;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-
-import java.util.List;
-import java.util.Objects;
 
 @Controller
 @RequiredArgsConstructor
@@ -23,19 +16,19 @@ public class IndexController {
         return "/";
     }
 
-    // 상품 검색
-    @GetMapping("/search")
-    public String searchProduct(ProductDto productDto, Model model) {
-        if (Objects.equals(productDto.getSearchCondition(), SearchCondition.TITLE)) productDto.setSearchCondition(SearchCondition.TITLE);
-        if (productDto.getSearchKeyword() == null) productDto.setSearchKeyword("");
-
-        model.addAttribute("condition", productDto.getSearchCondition());
-        model.addAttribute("keyword", productDto.getSearchKeyword());
-
-        List<Product> searchResult = productService.SearchProducts(productDto);
-
-        model.addAttribute("searchResult", searchResult);
-        return "redirect:/";
-    }
+//    // 상품 검색
+//    @GetMapping("/search")
+//    public String searchProduct(ProductDto productDto, Model model) {
+//        if (Objects.equals(productDto.getSearchCondition(), SearchCondition.TITLE)) productDto.setSearchCondition(SearchCondition.TITLE);
+//        if (productDto.getSearchKeyword() == null) productDto.setSearchKeyword("");
+//
+//        model.addAttribute("condition", productDto.getSearchCondition());
+//        model.addAttribute("keyword", productDto.getSearchKeyword());
+//
+//        List<Product> searchResult = productService.SearchProducts(productDto);
+//
+//        model.addAttribute("searchResult", searchResult);
+//        return "redirect:/";
+//    }
 
 }
