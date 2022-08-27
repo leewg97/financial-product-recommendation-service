aef286c837dac0de3b4cfda9350b1c0d58c1ec50
Chang1235 <dlckdgml1235@naver.com>
2022-08-27 15:11:13 +0900

    feat: Add 찜하기여부 확인 기능 구현

--- src/main/java/com/fastcampus/service/ProductService.java
+++ src/main/java/com/fastcampus/service/ProductService.java
@@ -1,7 +1,9 @@
 package com.fastcampus.service;
 
+import com.fastcampus.domain.Bookmark;
 import com.fastcampus.domain.Member;
 import com.fastcampus.domain.Product;
+import com.fastcampus.persistence.BookmarkRepository;
 import com.fastcampus.persistence.MemberRepository;
 import com.fastcampus.persistence.ProductRepository;
 import com.fastcampus.web.dto.ProductDto;
@@ -23,8 +25,25 @@ public class ProductService {
     private final ProductRepository productRepository;
     private final MemberRepository memberRepository;
 
+    private final BookmarkRepository bookmarkRepository;
+
+    // 찜여부
+    public boolean isBookmark(Long memberId, Long productId){
+        Member member = memberRepository.findById(memberId).orElseThrow();
+        List<Bookmark> bookmark = bookmarkRepository.findByMember(member);
+        for (int i = 0; i <bookmark.size() ; i++) {
+            // 찜한 제품 아이디 찾고
+            if(productId == bookmark.get(i).getProduct().getId()){
+                return true;
+            }else{
+                return false;
+            }
+        }
+        return false;
+    }
+
     // productResponseDTO 값 설정
-    public static List<ProductDto.Response> getProductResList(List<Product> productList){
+    public List<ProductDto.Response> getProductResList(List<Product> productList, Long memberId){
         List<ProductDto.Response> productDtoList = new ArrayList<>();
         for (int i = 0; i <productList.size() ; i++) {
             Product product = productList.get(i);
@@ -34,7 +53,8 @@ public class ProductService {
                     product.getProductContent(),
                     product.getSupporterName(),
                     product.getSupporterRegion(),
-                    product.getSupporterAmount()
+                    product.getSupporterAmount(),
+                    isBookmark(memberId,product.getId())
             );
             productDtoList.add(productResDto);
         }
@@ -43,29 +63,29 @@ public class ProductService {
 
     // 상품 검색
     @Transactional
-    public List<ProductDto.Response> SearchProducts(ProductDto.Request productDto) {
+    public List<ProductDto.Response> SearchProducts(ProductDto.Request productDto,Long memberId) {
         if (productDto.getSearchCondition().equals(SearchCondition.TITLE)) {
             List<Product> productList = productRepository.findByProductNameContaining(productDto.getSearchKeyword());
-            return getProductResList(productList);
+            return getProductResList(productList,memberId);
         } else if (productDto.getSearchCondition().equals(SearchCondition.CONTENT)) {
             List<Product> productList = productRepository.findByProductContentContaining(productDto.getSearchKeyword());
-            return getProductResList(productList);
+            return getProductResList(productList,memberId);
         }
         List<Product> productList = productRepository.findAll();
-        return getProductResList(productList);
+        return getProductResList(productList,memberId);
     }
 
     // 맞춤상품
     @Transactional
-    public List<ProductDto.Response> customProducts(Long id) throws Exception {
-        Optional<Member> member = memberRepository.findById(id);
+    public List<ProductDto.Response> customProducts(Long productId,Long memberId) throws Exception {
+        Optional<Member> member = memberRepository.findById(productId);
         if (member.isPresent()) {
             Member findMember = member.get();
             int amount = findMember.getHopeAmount();
             String region = findMember.getRegion();
             List<Product> productList = productRepository.findBySupporterAmountGreaterThanEqualAndSupporterRegion(amount, region);
             log.info(productList.toString());
-            return getProductResList(productList);
+            return getProductResList(productList,memberId);
         }else{
             throw new Exception("회원이 존재하지 않습니다.");
         }
@@ -73,8 +93,8 @@ public class ProductService {
 
     // 상품 조회
     @Transactional
-    public ProductDto.Response getProduct(Long id) throws Exception {
-        Optional<Product> findProduct =  productRepository.findById(id);
+    public ProductDto.Response getProduct(Long productId,Long memberId) throws Exception {
+        Optional<Product> findProduct =  productRepository.findById(productId);
         if(findProduct.isPresent()){
             Product product = findProduct.get();
             ProductDto.Response productDto = new ProductDto.Response(
@@ -83,7 +103,8 @@ public class ProductService {
                     product.getProductContent(),
                     product.getSupporterName(),
                     product.getSupporterRegion(),
-                    product.getSupporterAmount()
+                    product.getSupporterAmount(),
+                    isBookmark(memberId,product.getId())
             );
             return productDto;
         }else{
@@ -93,9 +114,9 @@ public class ProductService {
 
     // 상품 목록
     @Transactional
-    public List<ProductDto.Response> productList() {
+    public List<ProductDto.Response> productList(Long memberId) {
         List<Product> productList =productRepository.findAll();
-        return getProductResList(productList);
+        return getProductResList(productList,memberId);
     }
 
 }
--- src/main/java/com/fastcampus/web/controller/ProductController.java
+++ src/main/java/com/fastcampus/web/controller/ProductController.java
@@ -25,28 +25,28 @@ public class ProductController {
 
     // 상품 목록
     @GetMapping("/list")
-    public @ResponseBody ResponseEntity productList(){
-        return new ResponseEntity(DefaultRes.res(StatusCode.OK, ResponseMessage.GET_PRODUCT_LIST,productService.productList()), HttpStatus.OK);
+    public @ResponseBody ResponseEntity productList(Long memberId){
+        return new ResponseEntity(DefaultRes.res(StatusCode.OK, ResponseMessage.GET_PRODUCT_LIST,productService.productList(memberId)), HttpStatus.OK);
 
     }
 
     // 상품 추천
     @GetMapping("/recommend/{id}")
-    public @ResponseBody ResponseEntity customProduct(@ApiParam(value = "회원의아이디") @PathVariable Long id) throws Exception {
-        return new ResponseEntity(DefaultRes.res(StatusCode.OK, ResponseMessage.GET_CUSTOM_PRODUCT_LIST,productService.customProducts(id)), HttpStatus.OK);
+    public @ResponseBody ResponseEntity customProduct(@ApiParam(value = "회원의아이디") @PathVariable Long productId,Long memberId) throws Exception {
+        return new ResponseEntity(DefaultRes.res(StatusCode.OK, ResponseMessage.GET_CUSTOM_PRODUCT_LIST,productService.customProducts(productId,memberId)), HttpStatus.OK);
     }
 
 
     // 상품 검색
     @GetMapping("/search")
-    public @ResponseBody ResponseEntity searchProduct(ProductDto.Request productDto)  {
-        return new ResponseEntity(DefaultRes.res(StatusCode.OK, ResponseMessage.GET_PRODUCT_SEARCH,productService.SearchProducts(productDto)), HttpStatus.OK);
+    public @ResponseBody ResponseEntity searchProduct(ProductDto.Request productDto,Long memberId)  {
+        return new ResponseEntity(DefaultRes.res(StatusCode.OK, ResponseMessage.GET_PRODUCT_SEARCH,productService.SearchProducts(productDto,memberId)), HttpStatus.OK);
     }
 
     // 상품 조회
     @GetMapping("/{id}")
-    public @ResponseBody ResponseEntity getProduct(@ApiParam(value = "상품의 index") @PathVariable Long id) throws Exception {
-        return new ResponseEntity(DefaultRes.res(StatusCode.OK, ResponseMessage.GET_PRODUCT,productService.getProduct(id)), HttpStatus.OK);
+    public @ResponseBody ResponseEntity getProduct(@ApiParam(value = "상품의 index") @PathVariable Long ProductId,Long memberId) throws Exception {
+        return new ResponseEntity(DefaultRes.res(StatusCode.OK, ResponseMessage.GET_PRODUCT,productService.getProduct(ProductId,memberId)), HttpStatus.OK);
     }
 
 }
