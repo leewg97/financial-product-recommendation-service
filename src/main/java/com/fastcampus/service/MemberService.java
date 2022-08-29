24c8a175d2087df579ccf06979874ca8feb6afde
leewg97 <leewg97@naver.com>
2022-08-29 16:51:03 +0900

    feat : Modified Member Service 회원가입과 동시에 장바구니 생성 로직 구현

--- src/main/java/com/fastcampus/service/MemberService.java
+++ src/main/java/com/fastcampus/service/MemberService.java
@@ -1,6 +1,9 @@
 package com.fastcampus.service;
 
+import com.fastcampus.domain.Cart;
+import com.fastcampus.domain.CartProduct;
 import com.fastcampus.domain.Member;
+import com.fastcampus.persistence.CartRepository;
 import com.fastcampus.persistence.MemberRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -15,6 +18,7 @@ public class MemberService {
     // 생성자 주입
     private final MemberRepository memberRepository;
     private final PasswordEncoder passwordEncoder;
+    private final CartRepository cartRepository;
 
     // 회원 등록
     @Transactional
@@ -22,6 +26,14 @@ public class MemberService {
         member.setJob("소상공인");
         member.setPassword(passwordEncoder.encode(member.getPassword()));
         memberRepository.save(member);
+
+        // 회원가입과 동시에 장바구니 생성
+        Cart cart = cartRepository.findByMemberId(member.getId());
+        if (cart == null) { 
+            cart = Cart.addCart(member);
+            cartRepository.save(cart);
+        }
+
     }
 
     // 회원 검색
