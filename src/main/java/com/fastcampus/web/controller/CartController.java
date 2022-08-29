60eb49893666617abe3f9305b34048bd82505193
leewg97 <leewg97@naver.com>
2022-08-29 16:43:18 +0900

    feat : Modified Cart Controller 장바구니 등록, 수정, 삭제 로직 수정

--- src/main/java/com/fastcampus/web/controller/CartController.java
+++ src/main/java/com/fastcampus/web/controller/CartController.java
@@ -1,20 +1,24 @@
 package com.fastcampus.web.controller;
 
 import com.fastcampus.Security.auth.PrincipalDetails;
-import com.fastcampus.domain.Cart;
 import com.fastcampus.service.CartService;
 import com.fastcampus.web.api.DefaultRes;
 import com.fastcampus.web.api.ResponseMessage;
 import com.fastcampus.web.api.StatusCode;
+import com.fastcampus.web.dto.CartProductDto;
+import com.fastcampus.web.dto.ProductDto;
+import io.swagger.annotations.Api;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
+
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@Api(tags = {"장바구니 정보를 제공하는 Controller"})
 @Controller
 @RequestMapping("/carts")
 @RequiredArgsConstructor
@@ -22,49 +26,33 @@ public class CartController {
 
     private final CartService cartService;
 
-
     // 장바구니 등록
-//    @PostMapping("/{id}/cart")
-//    public @ResponseBody ResponseEntity addCart(@PathVariable Long id, @AuthenticationPrincipal PrincipalDetails principalDetails) {
-//        return new ResponseEntity(DefaultRes.res(StatusCode.OK, ResponseMessage.ADD_CART, cartService.addCart(principalDetails.getMember().getId(), id);), HttpStatus.OK);
-//    }
-
-    // 장바구니 등록
-    @PostMapping("/{memberId}/{productId}")
-    public @ResponseBody ResponseEntity addCart(@PathVariable Long productId, @PathVariable Long memberId) {
-        return new ResponseEntity(DefaultRes.res(StatusCode.OK, ResponseMessage.ADD_CART, cartService.addCart(memberId, productId)), HttpStatus.OK);
+    @PostMapping("/add")
+    public @ResponseBody ResponseEntity addCart(@RequestBody CartProductDto.Request cartProductDto, Authentication authentication) {
+        PrincipalDetails principal = (PrincipalDetails) authentication.getPrincipal();
+        Long memberId = principal.getMember().getId();
+        return new ResponseEntity(DefaultRes.res(StatusCode.OK, ResponseMessage.ADD_CART, cartService.addCart(cartProductDto, memberId)), HttpStatus.OK);
     }
 
     // 장바구니 조회
-//    @GetMapping("/cart")
-//    public @ResponseBody ResponseEntity getCart(@AuthenticationPrincipal PrincipalDetails principalDetails) {
-//        return new ResponseEntity(DefaultRes.res(StatusCode.OK, ResponseMessage.READ_CART, cartService.findCarts(principalDetails.getMember())), HttpStatus.OK);
-//    }
-
-    // 장바구니 조회
     @GetMapping("/find")
-    public @ResponseBody ResponseEntity getCart(Authentication authentication) {
+    public @ResponseBody ResponseEntity getCart(Authentication authentication) throws Exception {
         PrincipalDetails principal = (PrincipalDetails) authentication.getPrincipal();
-        long memberId = principal.getMember().getId();
-        List<Cart> findCart = cartService.findCarts(memberId);
+        Long memberId = principal.getMember().getId();
+
+        List<ProductDto.Response> findCart = cartService.findCarts(memberId);
 
         if (!findCart.isEmpty()) {
             return new ResponseEntity(DefaultRes.res(StatusCode.OK, ResponseMessage.READ_CART, cartService.findCarts(memberId)), HttpStatus.OK);
         }
-        return new ResponseEntity(DefaultRes.res(StatusCode.OK, ResponseMessage.NOT_FOUNT_CART, "장바구니 목록이 존재하지 않습니다."), HttpStatus.OK);
+       return new ResponseEntity(DefaultRes.res(StatusCode.OK, ResponseMessage.NOT_FOUNT_CART, "장바구니 목록이 존재하지 않습니다."), HttpStatus.OK);
     }
 
     // 장바구니 삭제
-    @DeleteMapping("/{cartId}")
-    public @ResponseBody ResponseEntity deleteCart(@PathVariable Long cartId) {
-        cartService.deleteCart(cartId);
+    @DeleteMapping("/{id}")
+    public @ResponseBody ResponseEntity deleteCart(@PathVariable Long id) {
+        cartService.deleteCartProduct(id);
         return new ResponseEntity(DefaultRes.res(StatusCode.OK, ResponseMessage.DELETE_CART, "장바구니 목록이 삭제 되었습니다."), HttpStatus.OK);
     }
 
-    // 장바구니 요청
-//    @PostMapping("/ask")
-//    public @ResponseBody ResponseEntity askCart(@AuthenticationPrincipal PrincipalDetails principalDetails) {
-//        cartService.askCart(principalDetails.getMember());
-//        return new ResponseEntity(DefaultRes.res(StatusCode.OK, ResponseMessage.ASK_CART), HttpStatus.OK);
-//    }
 }
