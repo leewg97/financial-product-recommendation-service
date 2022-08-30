6654441e746f35dda9548c2835bb233abfcbbf97
leewg97 <leewg97@naver.com>
2022-08-30 14:18:48 +0900

    feat : Modified CartRepository import 수정

--- src/main/java/com/fastcampus/persistence/CartRepository.java
+++ src/main/java/com/fastcampus/persistence/CartRepository.java
@@ -1,7 +1,6 @@
 package com.fastcampus.persistence;
 
 import com.fastcampus.domain.Cart;
-import com.fastcampus.domain.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
