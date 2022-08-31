2c6250b41139f25aea8893c0cfa5decefb691bfe
Chang1235 <dlckdgml1235@naver.com>
2022-08-31 19:54:17 +0900

    refactor: Refactor config file

--- src/main/java/com/fastcampus/config/CorsConfig.java
+++ src/main/java/com/fastcampus/config/CorsConfig.java
@@ -5,20 +5,32 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class CorsConfig {
+public class CorsConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedMethods("GET", "POST", "PUT", "PATCH", "OPTIONS")
+                .allowedHeaders("headers")
+                .maxAge(3000);
+    }
 
     @Bean
     public static CorsFilter corsFilter() {
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowCredentials(true); // 내 서버가 응답을 할 때 json을 자바스크립트에서 처리할 수 있게 할지를 설정
-        config.addAllowedOrigin("*"); // 모든 ip에 응답을 허용
+        config.setAllowCredentials(true); // 내서버가 응답을 할 때 json을 자바스크립트에서 처리할 수 있게 할지를 설정
+        config.addAllowedOriginPattern("*"); // 모든 ip에 응답을 허용
         config.addAllowedHeader("*"); // 모든 header에 응답을 허용
-        config.addAllowedMethod("*"); // 모든 api 요청을 허용
+        config.addAllowedMethod("*"); // 모든 post,get,put,delete,patch 요청을 허용
+        config.addExposedHeader("Authorization");
+        source.registerCorsConfiguration("/**", config);
 
-        source.registerCorsConfiguration("/api/**", config);
         return new CorsFilter(source);
     }
 
