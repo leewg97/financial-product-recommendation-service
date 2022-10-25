52c88233a36628bca83d26a830553c039af6e771
leewg97 <leewg97@naver.com>
2022-10-25 15:44:22 +0900

    feat : Add HealthCheckController

---
+++ src/main/java/com/fastcampus/web/controller/HealthCheckController.java
@@ -0,0 +1,14 @@
+package com.fastcampus.web.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthCheckController {
+
+    @GetMapping("/health")
+    public String health() {
+        return "health ok";
+    }
+
+}
